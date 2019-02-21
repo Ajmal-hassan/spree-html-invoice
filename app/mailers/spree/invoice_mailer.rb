9cd5fe9a8db0e3aa1c3e8879caee2f73c7ef758d
Duc T. Lam <ducl_13@icloud.com>
2019-02-21 11:10:33 -0800

    Different headers. Show shipments, payment info. Add mailer.

---
+++ app/mailers/spree/invoice_mailer.rb
@@ -0,0 +1,41 @@
+module Spree
+  class InvoiceMailer < BaseMailer
+    def invoice_email(order)
+      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+      to_address = Rails.configuration.x.backoffice.to_address
+      subject = "[INVOICE] Order #: #{@order.number}"
+
+      attachments["invoice-#{@order.number}.pdf"] = WickedPdf.new.pdf_from_string(
+        get_invoice_content("invoice", @order)
+      )
+
+      mail(to: to_address, from: from_address, subject: subject)
+    end
+
+    def packaging_slip_email(order)
+      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+      to_address = Rails.configuration.x.backoffice.to_address
+      subject = "[PACKAGING SLIP] Order #: #{@order.number}"
+      
+      attachments["packaging_slip-#{@order.number}.pdf"] = WickedPdf.new.pdf_from_string(
+        get_packaging_list_content("packaging_slip", @order)
+      )
+      
+      mail(to: to_address, from: from_address, subject: subject)
+    end
+
+    private
+
+    def get_invoice_content(template, order)
+      @template = template
+      @order = order
+      render_to_string(:template => "spree/invoice_mailer/invoice.html.erb", :layout => false)
+    end
+
+    def get_packaging_list_content(template, order)
+      @template = template
+      @order = order
+      render_to_string(:template => "spree/invoice_mailer/packaging_slip.html.erb", :layout => false)
+    end
+  end
+end
---
+++ app/views/spree/invoice_mailer/README.md
@@ -0,0 +1,4 @@
+# Duplicate from spree_html_invoice: views/spree/admin/invoice
+# - must replace params[:template] with @template in the files.
+#
+# There are also overrides in views/spree/admin/invoice directory in this project.

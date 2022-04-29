809f5f73cac3007985c8034f92b006f2a924c62c
Duc T. Lam <ducl_13@yahoo.com>
2022-04-29 13:55:35 -0700

    Hot fixes

--- app/overrides/add_html_invoice_buttons.rb
+++ app/overrides/add_html_invoice_buttons.rb
@@ -1,7 +1,6 @@
 Deface::Override.new(
   virtual_path: "spree/admin/shared/_order_tabs",
   name: "add_html_invoice_buttons",
-  insert_bottom: '[data-hook="admin_order_tabs_cart_details"]',
-  partial: "spree/admin/orders/html_buttons",
-  disabled: false
+  insert_after: '[data-hook="admin_order_tabs_state_changes"]',
+  partial: "spree/admin/orders/html_buttons"
 )

db7ab9028e48d59020f2b6edbb5a378b425b5125
Duc T. Lam <ducl_13@yahoo.com>
2023-02-22 14:35:40 -0800

    Change print html to download pdf

--- app/controllers/spree/admin/invoice_controller.rb
+++ app/controllers/spree/admin/invoice_controller.rb
@@ -7,9 +7,19 @@ module Spree
     class InvoiceController < Spree::BaseController
       def show
         template = params[:template]
-        instance_variable_set('@' + template, true)
+        @template = template
         @order = Spree::Order.find_by(number: params[:id])
-        render template, layout: false
+        pdf = WickedPdf.new.pdf_from_string(
+          render_to_string(:template => "spree/admin/invoice/#{template}.html.erb", :layout => false),
+          dpi: 300,
+          :page_size => 'Letter',
+          footer: {
+            right: "Order #: #{@order.number}",
+            font_name: 'helvetica, verdana, serif',
+            font_size: 6
+          }
+        )
+        send_data pdf, type: "application/pdf", filename: "#{@order.number}-#{template}.pdf", :disposition => 'attachment'
       end
     end
   end

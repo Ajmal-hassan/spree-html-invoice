3ad75a182ffc45c01b99a2f7b04b0ddde5fb0225
Duc T. Lam <ducl_13@yahoo.com>
2024-02-22 22:38:09 -0800

    Print return authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Spree::Core::Engine.routes.append do
   namespace :admin do
     get 'invoice/:id(/:template)', to: 'invoice#show'
+    get 'orders/:order_id/return_authorizations/:id/print', to: 'return_authorizations#print'
   end
 end

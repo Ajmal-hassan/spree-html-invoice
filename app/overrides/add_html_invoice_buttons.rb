2c2457ff30c8d8185bd4a7ade77cfc1beb652475
Ajmal hassan <ajmalhassan651@gmail.com>
2024-12-26 16:10:08 +0500

    Delete app/overrides directory
    Delete overrides  for spree4.8

--- app/overrides/add_html_invoice_buttons.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: "spree/admin/shared/_order_tabs",
-  name: "add_html_invoice_buttons",
-  insert_after: '[data-hook="admin_order_tabs_state_changes"]',
-  partial: "spree/admin/orders/html_buttons"
-)
--- app/overrides/add_ra_print_button.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: "spree/admin/return_authorizations/index",
-  name: "add_ra_print_button",
-  insert_bottom: 'td.actions span',
-  partial: "spree/admin/return_authorizations/buttons"
-)
